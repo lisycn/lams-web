c36faf318a7b56dc1063bc1b4c19ea54b6143c08
HarshitCW <harshit.suhagiya@capitaworld.com>
2018-03-07 23:14:17 +0530

    - add new fields

--- src/main/resources/static/application/applicationCtrl.js
+++ src/main/resources/static/application/applicationCtrl.js
@@ -1,3 +1,6 @@
-app.controller("applicationCtrl",["$scope",function(){
-	
+app.controller("applicationCtrl",["$scope",function($scope){
+	$scope.selectedTab = 0;
+	$scope.changeTab = function(tab) {
+		$scope.selectedTab = parseInt(tab) - 1;
+	}
 }]);
