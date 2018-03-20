da7fe3c19d14215ace289ffd6296469fbf126c67
AkshayCW <akshay.patel@capitaworld.com>
2018-03-21 02:28:17 +0530

    Borrowers List for Lender

--- src/main/resources/static/dashboard/ldDashboardCtrl.js
+++ src/main/resources/static/dashboard/ldDashboardCtrl.js
@@ -1,3 +1,25 @@
-angular.module("lams").controller("ldDashboardCtrl",["$scope", "$http","$rootScope","Constant",
-		function($scope, $rootScope,Constant) {
+angular.module("lams").controller("ldDashboardCtrl",["$scope", "$rootScope","Constant","applicationService",
+		function($scope,$rootScope,Constant,applicationService) {
+
+	$scope.borrowers = [];
+	$scope.getBorrowers = function() {
+		applicationService.getBorrowerForLender().then(
+			function(success) {
+				if (success.data.status == 200) {
+					if(!$rootScope.isEmpty(success.data.data)){
+						$scope.borrowers = success.data.data;
+						console.log("$scope.users==>", $scope.borrowers);						
+					}
+				} else if (success.data.status == 400) {
+					Notification.error(success.data.message);
+				} else {
+					Notification.error(Constant.ErrorMessage.SOMETHING_WENT_WRONG);
+				}
+			}, function(error) {
+				$rootScope.validateErrorResponse(error);
+			});
+	}
+	
+	$scope.getBorrowers();
+
 }]);
